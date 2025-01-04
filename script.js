77e1647ef4c8f23600116036b0875979ae9ae439
duonisme <153653712+duonisme@users.noreply.github.com>
2025-01-04 11:45:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+  document.querySelector('.nav-menu').classList.toggle('open');
+});
+
+
+
+<script src="script.js"></script>
