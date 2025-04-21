84838a963812d9f2cb37a9152af2c6b5ee84ee17
duonisme <153653712+duonisme@users.noreply.github.com>
2025-04-21 19:07:23 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-  document.querySelector('.nav-menu').classList.toggle('open');
-});
-
-
-
-<script src="script.js"></script>
